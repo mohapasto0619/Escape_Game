3526c6e96e5a75f866e7fc9b273f68863e272e3f
kalidation <kalidation@gmail.com>
2021-10-16 01:52:47 +0200

    Create User in Firestore Done. Create animation after creating User. By khaled

--- app/src/main/java/fr/mastergime/meghasli/escapegame/repositories/GlobalRepository.kt
+++ app/src/main/java/fr/mastergime/meghasli/escapegame/repositories/GlobalRepository.kt
@@ -2,7 +2,11 @@ package fr.mastergime.meghasli.escapegame.repositories
 
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.FirebaseNetworkException
 import fr.mastergime.meghasli.escapegame.backend.AuthServiceFirebase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import java.io.IOException
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,9 +14,13 @@ import javax.inject.Singleton
 class GlobalRepository @Inject constructor (val authServiceFirebase : AuthServiceFirebase){
 
 
+    private val _requestState = MutableStateFlow(MessageResult.START)
+    val requestState: Flow<MessageResult> get() = _requestState
 
-   suspend  fun signUp (email : String , password : String , pseudo :String) : String {
-        return authServiceFirebase.signup(email, password,pseudo)
+
+   suspend  fun signUp (email : String , password : String , pseudo :String): String {
+     //  _requestState.emit(MessageResult.START)
+       return    authServiceFirebase.signup(email, password,pseudo)
     }
 
      fun login (email : String , password : String)  {
@@ -20,7 +28,9 @@ class GlobalRepository @Inject constructor (val authServiceFirebase : AuthServic
     }
 
 
-
+    enum class MessageResult{
+        START,SUCCESS,FAILED
+    }
 
 
 
