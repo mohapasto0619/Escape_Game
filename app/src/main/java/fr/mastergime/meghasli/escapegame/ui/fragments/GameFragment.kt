692039e66f6e6cefbb09328cf89fc12c55811699
Felouah Yacine <felouahyacine@gmail.com>
2021-11-06 02:00:16 +0100

    NFC joinSession ok EmulateCard ok ActivateEnigme ok v0.2

--- app/src/main/java/fr/mastergime/meghasli/escapegame/ui/fragments/GameFragment.kt
+++ app/src/main/java/fr/mastergime/meghasli/escapegame/ui/fragments/GameFragment.kt
@@ -1,6 +1,11 @@
 package fr.mastergime.meghasli.escapegame.ui.fragments
 
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
+import android.nfc.tech.Ndef
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,17 +14,24 @@ import android.widget.MediaController
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import fr.mastergime.meghasli.escapegame.R
 import fr.mastergime.meghasli.escapegame.databinding.FragmentCreatSessionBinding
 import fr.mastergime.meghasli.escapegame.databinding.FragmentGameBinding
+import fr.mastergime.meghasli.escapegame.model.ReaderMode
+import fr.mastergime.meghasli.escapegame.model.Utils
 import fr.mastergime.meghasli.escapegame.viewModels.SessionViewModel
 import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 @AndroidEntryPoint
-class GameFragment : Fragment() {
-    val sessionViewModel : SessionViewModel by viewModels()
-    lateinit var binding : FragmentGameBinding
+class GameFragment : Fragment(),   NfcAdapter.ReaderCallback {
+    val sessionViewModel: SessionViewModel by viewModels()
+    var mNfcAdapter: NfcAdapter? = null
+    lateinit var binding: FragmentGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,41 +50,41 @@ class GameFragment : Fragment() {
 
         sessionViewModel.updateSessionId()
 
-        binding.quitButton.setOnClickListener{
+        binding.quitButton.setOnClickListener {
             binding.progressBar.visibility = View.VISIBLE
             it.isEnabled = false
             sessionViewModel.quitSession()
         }
 
-        sessionViewModel.quitSessionState.observe(viewLifecycleOwner){value ->
-            if(value == "Success")
+        sessionViewModel.quitSessionState.observe(viewLifecycleOwner) { value ->
+            if (value == "Success")
                 findNavController().navigate(R.id.action_gameFragment_to_menuFragment)
             else
-                Toast.makeText(activity,"Can't leave Session please retry",
+                Toast.makeText(activity, "Can't leave Session please retry",
                     Toast.LENGTH_SHORT).show()
 
             binding.progressBar.visibility = View.INVISIBLE
             binding.quitButton.isEnabled = true
         }
 
-        sessionViewModel.sessionId.observe(viewLifecycleOwner){
+        sessionViewModel.sessionId.observe(viewLifecycleOwner) {
             sessionId = it
         }
 
         // le tag nfc de l'Enigme 1
         binding.btnEnigma1.setOnClickListener {
             val bundle = bundleOf("enigmeTag" to "enigme1")
-            findNavController().navigate(R.id.action_gameFragment_to_enigme1Fragment,bundle)
+            findNavController().navigate(R.id.action_gameFragment_to_enigme1Fragment, bundle)
         }
         // le tag nfc de l'Enigme 2   partie1
         binding.btnEnigma21.setOnClickListener {
             val bundle = bundleOf("enigmeTag" to "enigme2")
-            findNavController().navigate(R.id.action_gameFragment_to_enigme21Fragment,bundle)
+            findNavController().navigate(R.id.action_gameFragment_to_enigme21Fragment, bundle)
         }
         // le tag nfc de l'Enigme 2   partie2
         binding.btnEnigma22.setOnClickListener {
             val bundle = bundleOf("enigmeTag" to "enigme2")
-            findNavController().navigate(R.id.action_gameFragment_to_enigme22Fragment,bundle)
+            findNavController().navigate(R.id.action_gameFragment_to_enigme22Fragment, bundle)
         }
         // le tag nfc de l'Enigme 3
         binding.btnEnigma3.setOnClickListener {
@@ -82,13 +94,11 @@ class GameFragment : Fragment() {
         // le tag nfc de l'Enigme4
         binding.btnEnigma4.setOnClickListener {
             val bundle = bundleOf("enigmeTag" to "enigme4")
-           // findNavController().navigate(R.id.action_gameFragment_to_enigme1Fragment,bundle)
+            // findNavController().navigate(R.id.action_gameFragment_to_enigme1Fragment,bundle)
         }
 
 
-
-
-       /* val onlineUri = Uri.parse("https://www.youtube.com/watch?v=Jd3nTm-wvyA&ab_channel=CodePalace")
+        /* val onlineUri = Uri.parse("https://www.youtube.com/watch?v=Jd3nTm-wvyA&ab_channel=CodePalace")
         val offlineUri = Uri.parse("android.resource://"+requireActivity().packageName+"/"+R.raw.video_test)
        // binding.videoView.setMediaController(mediaController)
         binding.videoView.setVideoURI(offlineUri)
@@ -102,6 +112,64 @@ class GameFragment : Fragment() {
         }*/
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        enableNfc()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(context)
+        mNfcAdapter!!.disableReaderMode(activity)
+    }
+
+    private fun enableNfc() {
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(context)
+        if (mNfcAdapter != null && mNfcAdapter!!.isEnabled) {
+
+            mNfcAdapter!!.enableReaderMode(
+                this.activity,
+                this,
+                NfcAdapter.FLAG_READER_NFC_A, Bundle.EMPTY
+            )
+        } else {
+        }
+    }
+
+    override fun onTagDiscovered(tag: Tag?) {
+
+        val mNdef: Ndef? = Ndef.get(tag)
+
+        if (mNdef != null) {
+            mNdef.connect()
+            val mNdefMessage = mNdef.ndefMessage
+            val msg = mNdefMessage.records[0].toUri().toString()
+
+            when (msg) {
+                "enigme1" -> {
+                    lifecycleScope.launch(Dispatchers.Main) {
+                        val bundle = bundleOf("enigmeTag" to "enigme1")
+                        findNavController().navigate(R.id.action_gameFragment_to_enigme1Fragment, bundle)
+                    }
+
+                }
+                "enigme2" -> {
+                    lifecycleScope.launch(Dispatchers.Main) {
+                        val bundle = bundleOf("enigmeTag" to "enigme2")
+                        findNavController().navigate(R.id.action_gameFragment_to_enigme21Fragment, bundle)
+                    }
+                }
+            }
+
+            mNdef.close()
+        } else {
+            ReaderMode.message = "FAILED"
+        }
+    }
+
+
     companion object {
 
         var sessionId = ""
