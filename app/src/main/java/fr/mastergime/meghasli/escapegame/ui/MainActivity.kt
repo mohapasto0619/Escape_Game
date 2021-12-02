08e730e52a827320d32fa4856123a7f443baba36
aymanedkhissirevo <Elle3Aymanio>
2021-12-02 14:42:09 +0100

    comm

--- app/src/main/java/fr/mastergime/meghasli/escapegame/ui/MainActivity.kt
+++ app/src/main/java/fr/mastergime/meghasli/escapegame/ui/MainActivity.kt
@@ -24,7 +24,7 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
+//
     private lateinit var _binding: ActivityMainBinding
     private val splashFragment = SplashFragment()
     private lateinit var appBarConfiguration: AppBarConfiguration
