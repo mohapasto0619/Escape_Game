f97ff848f618989dab5f9402a85cfeaea30f5c77
aymanedkhissirevo <Elle3Aymanio>
2021-12-02 13:54:38 +0100

    login

--- app/src/main/java/fr/mastergime/meghasli/escapegame/ui/fragments/GameFragment.kt
+++ app/src/main/java/fr/mastergime/meghasli/escapegame/ui/fragments/GameFragment.kt
@@ -155,7 +155,7 @@ class GameFragment : Fragment(), NfcAdapter.ReaderCallback {
             /***end code bluetooth**/
         }
 
-
+// comment
 
         sessionViewModel.quitSessionState.observe(viewLifecycleOwner) { value ->
             observeSessionState(value)
